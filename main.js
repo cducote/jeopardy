c42e989abd48050844db613287084c81d456d9b5
Chris Ducote <chrisd@slappy.local>
2018-09-05 13:16:11 -0400

    400 questions and answers set

--- main.js
+++ main.js
@@ -24,6 +24,56 @@ const gameBoard = {
         value: 200,
         category: "JS"
            
+    }],
+    fourHundred: [{
+        question: 'This tag is wrapped around your links to stylesheets',
+        right: '< head >',
+        wrong: ["< body >", "< header >", "< div >"],
+        value: 400,
+        category: "HTML"
+    }, {
+        question: 'This property exists between the Border and its Element',
+        right: 'padding:',
+        wrong: ["margin:", "border:", "bald-spot:"],
+        value: 400,
+        category: "CSS"
+    }, {
+        question: 'This bash command moves the user from directory to directory',
+        right: 'cd',
+        wrong: ["pwd", "ls", "mkdir"],
+        value: 400,
+        category: "Terminal"
+    }, {
+        question: 'Three letter word that starts off an iteration loop',
+        right: 'for',
+        wrong: ["let", "loo", "var"],
+        value: 400,
+        category: "JS"
+    }],
+    sixHundred: [{
+        question: '',
+        right: '',
+        wrong: ["", "", ""],
+        value: 600,
+        category: ""
+    }, {
+        question: '',
+        right: '',
+        wrong: ["", "", ""],
+        value: 600,
+        category: ""
+    }, {
+        question: '',
+        right: '',
+        wrong: ["", "", ""],
+        value: 600,
+        category: ""
+    }, {
+        question: '',
+        right: '',
+        wrong: ["", "", ""],
+        value: 600,
+        category: ""
     }]
     
 }
@@ -34,9 +84,36 @@ $('#200').on('click', '.column', (e) => {
         $('.modal-card-title').append(`${question.category} for ${question.value}`)
         $('.modal-card-body').append(`${question.question}`)
         $('.A').append(`${question.right}`)
-        // $('.A').append(e.curretTarget.right)
+        $('.B').append(`${question.wrong[0]}`)
+        $('.C').append(`${question.wrong[1]}`)
+        $('.D').append(`${question.wrong[2]}`)
+})
+$('#400').on('click', '.column', (e) => {
+    const index = $(e.currentTarget).data('questionindex')
+    const question = gameBoard.fourHundred[index]
+    $('.modal').toggleClass('is-active')
+        $('.modal-card-title').append(`${question.category} for ${question.value}`)
+        $('.modal-card-body').append(`${question.question}`)
+        $('.A').append(`${question.right}`)
+        $('.B').append(`${question.wrong[0]}`)
+        $('.C').append(`${question.wrong[1]}`)
+        $('.D').append(`${question.wrong[2]}`)
+})
+$('#400').on('click', '.column', (e) => {
+    const index = $(e.currentTarget).data('questionindex')
+    const question = gameBoard.fourHundred[index]
+    $('.modal').toggleClass('is-active')
+        $('.modal-card-title').append(`${question.category} for ${question.value}`)
+        $('.modal-card-body').append(`${question.question}`)
+        $('.A').append(`${question.right}`)
         $('.B').append(`${question.wrong[0]}`)
         $('.C').append(`${question.wrong[1]}`)
         $('.D').append(`${question.wrong[2]}`)
 
-})
+
+        // copy and paste keys
+        // question: '',
+        // right: '',
+        // wrong: ["", "", ""],
+        // value: ,
+        // category: ""
